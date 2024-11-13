64831d4e788d0824bbd3c4dc2ad63f5de3e52d27
cannelle garcia <cannellegarcia@MacBook-Air-de-cannelle.local>
2024-11-13 16:57:13 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+
+import TheWelcome from './components/TheWelcome.vue'
+import logo1 from '/logo1.png';
+import logo2 from '/logo2.png';
+import CannelleGarcia from '/CannelleGarcia.png';
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo1" src="../public/logo1.png"/>
+    <img alt="Vue logo" class="logo2" src="../public/logo2.png"/> 
+    
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 1rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo1
+{
+  /* margin: 1rem 0; */
+  width : 100%;
+  height : auto;
+  display: block;
+  margin: 0 auto 1rem;
+}
+
+.logo2
+{
+  /* margin: 1rem 0; */
+  width : 50%;
+  height : auto;
+  display: block;
+  margin: 0 auto 1rem;
+}
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  } 
+}
+</style>
+
+
+
+
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,78 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+ 
+ <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Mon CV</template>
+
+    Téléchargez mon 
+    <a href="/CannelleGarcia.png" download>CV</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Mon Github</template>
+
+  
+    Pour retrouver mes projets collectifs, individuels et mes katas : <a href="https://github.com/cannellette" target="_blank" rel="noopener">C'est par ici que ça se passe</a>!
+
+    <br />
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Linkedin</template>
+
+    Retrouvez-moi sur<a href="https://www.linkedin.com/in/cannelle-garcia/" target="_blank" rel="noopener">Linkedin </a>! 
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Mes projets</template>
+
+    Retrouvez ici mes<a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">projets</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Contact</template>
+
+    Contactez-moi <a href="mailto:cannelle.garcia@gmail.com" target="_blank" rel="noopener">par mail</a> !
+  </WelcomeItem>
+
+
+
+
+</template>
+<script>
+export default {
+  methods: {
+    openAndDownloadImage(event) {
+      event.preventDefault();
+      const imageUrl = './assets/CannelleGarcia.png';
+      window.open(imageUrl, '_blank');
+      location.href = imageUrl;
+    },
+  },
+};
+</script>
